86fa3b9f47146ae4db505760ae48b3008d69c918
rsinukov <97PUsuqG>
2015-12-08 06:47:50 +0300

    processor classes

--- annotations/src/main/java/com/rsinukov/activityresult/annotations/ActivityResult.java
+++ annotations/src/main/java/com/rsinukov/activityresult/annotations/ActivityResult.java
@@ -1,5 +1,8 @@
 package com.rsinukov.activityresult.annotations;
 
+import com.rsinukov.activityresult.CustomParcel;
+import com.rsinukov.activityresult.EmptyParcel;
+
 /**
  * Created by rstk on 12/8/15.
  */
@@ -8,4 +11,5 @@ public @interface ActivityResult
     String name();
     Class type();
     boolean isRequired() default true;
+    Class<? extends CustomParcel> parcel() default EmptyParcel.class;
 }
