715232082a40c507bb4a38871fb9f5165aeefe1f
rsinukov <rxsinukov@gmail.com>
2016-03-03 03:46:18 +0300

    Custm bundler functionality

--- annotations/src/main/java/com/rsinukov/activityresult/CustomBundler.java
+++ annotations/src/main/java/com/rsinukov/activityresult/CustomBundler.java
@@ -6,6 +6,6 @@ public interface CustomBundler<T> {
 
     void put(String key, T value, Bundle bundle);
 
-    <V extends T> V get(String key, Bundle bundle);
+    <T> T get(String key, Bundle bundle);
 
 }
